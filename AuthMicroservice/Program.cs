08fd5ce31350bd2d636098ae938ea4173c1f5958
Chau Denis <chaudenisbreiz@yahoo.fr>
2025-06-20 14:25:36 +0200

    AuthMicroservice: Add Program.cs for application startup

--- AuthMicroservice/Program.cs
+++ AuthMicroservice/Program.cs
@@ -1,3 +1,17 @@
+using AuthMicroservice.Application.Interfaces;
+using AuthMicroservice.Application.UseCases;
+using AuthMicroservice.Infrastructure.Config;
+using AuthMicroservice.Infrastructure.Mapping;
+using AuthMicroservice.Infrastructure.Persistence;
+using AuthMicroservice.Infrastructure.Persistence.Entities;
+using AuthMicroservice.Infrastructure.Security;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using AuthMicroservice.Extensions;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,7 +21,44 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// DbContext SQL Server 
+builder.Services.AddDbContext<AuthDbContext>(opts =>
+    opts.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Configurations
+builder.Services.AddAuthMicroserviceServices(builder.Configuration);
+
+
+// Authentification JWT
+var jwtConfig = builder.Configuration.GetSection("Jwt").Get<JwtSettings>()!;
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(opt =>
+    {
+        opt.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = jwtConfig.Issuer,
+            ValidAudience = jwtConfig.Audience,
+            IssuerSigningKey = new SymmetricSecurityKey(
+                                          Encoding.UTF8.GetBytes(jwtConfig.Secret))
+        };
+    });
+builder.Services.AddAuthorization();
+
 var app = builder.Build();
+
+// Seeder Admin
+using (var scope = app.Services.CreateScope())
+{
+    var seeder = scope.ServiceProvider.GetRequiredService<SeedAdminUseCase>();
+    await seeder.ExecuteAsync(
+        builder.Configuration["Admin:Email"]!,
+        builder.Configuration["Admin:Password"]!
+    );
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
