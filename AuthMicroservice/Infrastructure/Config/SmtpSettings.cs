c77e29bae5137cb3f7ed37b8498a54173c32c2ec
Chau Denis <chaudenisbreiz@yahoo.fr>
2025-06-20 14:24:36 +0200

    AuthMicroservice: Add SmtpSettings configuration class

--- AuthMicroservice/Infrastructure/Config/SmtpSettings.cs
+++ AuthMicroservice/Infrastructure/Config/SmtpSettings.cs
@@ -2,5 +2,12 @@
 {
     public class SmtpSettings
     {
+        public string Host { get; set; } = null!;
+        public int Port { get; set; }
+        public bool EnableSsl { get; set; }
+        public string User { get; set; } = null!;
+        public string Password { get; set; } = null!;
+        public string From { get; set; } = null!;
+        public string FrontendUrl { get; set; } = null!;
     }
 }
