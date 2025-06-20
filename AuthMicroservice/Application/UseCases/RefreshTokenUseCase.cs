12b7df0eaba2305f56ac1e5914dc5a7d2f15e2db
Chau Denis <chaudenisbreiz@yahoo.fr>
2025-06-20 11:53:19 +0200

    AuthMicroservice: Add RefreshTokenUseCase for token renewal

--- AuthMicroservice/Application/UseCases/RefreshTokenUseCase.cs
+++ AuthMicroservice/Application/UseCases/RefreshTokenUseCase.cs
@@ -1,6 +1,23 @@
-﻿namespace AuthMicroservice.Application.UseCases
+﻿using AuthMicroservice.Application.Interfaces;
+using AuthMicroservice.Application.DTOs;
+
+namespace AuthMicroservice.Application.UseCases
 {
+    /// <summary>
+    /// Handles JWT refresh token flow.
+    /// </summary>
     public class RefreshTokenUseCase
     {
+        private readonly IAuthService _auth;
+
+        public RefreshTokenUseCase(IAuthService auth) => _auth = auth;
+
+        /// <summary>
+        /// Returns a new access token using the given refresh token.
+        /// </summary>
+        /// <param name="refreshToken">The refresh token.</param>
+        /// <returns>New JWT token if the refresh token is valid.</returns>
+        public Task<JwtResponse?> ExecuteAsync(string refreshToken) =>
+            _auth.RefreshTokenAsync(refreshToken);
     }
 }
