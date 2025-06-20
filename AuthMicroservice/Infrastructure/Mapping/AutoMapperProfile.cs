5e4d4c3277de13976af636418e0a3ea7f06607e6
Chau Denis <chaudenisbreiz@yahoo.fr>
2025-06-20 14:24:59 +0200

    AuthMicroservice: Add AutoMapperProfile for object mapping

--- AuthMicroservice/Infrastructure/Mapping/AutoMapperProfile.cs
+++ AuthMicroservice/Infrastructure/Mapping/AutoMapperProfile.cs
@@ -1,6 +1,17 @@
-﻿namespace AuthMicroservice.Infrastructure.Mapping
+﻿using AutoMapper;
+using AuthMicroservice.Application.DTOs;
+using AuthMicroservice.Infrastructure.Persistence.Entities;
+
+namespace AuthMicroservice.Infrastructure.Mapping
 {
-    public class AutoMapperProfile
+    public class AutoMapperProfile : Profile
     {
+        public AutoMapperProfile()
+        {
+            // Mappe User -> ProfileDto automatiquement
+            CreateMap<User, ProfileDto>();
+            CreateMap<EmailToken, EmailTokenDto>();
+            CreateMap<EmailTokenDto, EmailToken>();
+        }
     }
 }
