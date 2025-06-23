1bf2b1c0cc575525e90461ca4768dbbc519e3491
Chau Denis <chaudenisbreiz@yahoo.fr>
2025-06-23 10:01:59 +0200

    feat(card): Add GetAllCardsUseCase for listing all cards

--- CardMicroservice/Application/UseCases/GetAllCardsUseCase.cs
+++ CardMicroservice/Application/UseCases/GetAllCardsUseCase.cs
@@ -1,6 +1,13 @@
-﻿namespace CardMicroservice.Application.UseCases
+﻿using CardMicroservice.Application.DTOs;
+using CardMicroservice.Application.Interfaces;
+
+namespace CardMicroservice.Application.UseCases
 {
     public class GetAllCardsUseCase
     {
+        private readonly ICardRepository _repo;
+        public GetAllCardsUseCase(ICardRepository repo) => _repo = repo;
+
+        public Task<List<CardDto>> ExecuteAsync() => _repo.GetAllAsync();
     }
 }
