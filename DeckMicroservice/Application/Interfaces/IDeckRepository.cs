8e6b62b0ac790b11bc32a965bfc9678bce1e7b57
Chau Denis <chaudenisbreiz@yahoo.fr>
2025-06-23 12:58:27 +0200

    feat(deck): Add IDeckRepository interface for deck management

--- DeckMicroservice/Application/Interfaces/IDeckRepository.cs
+++ DeckMicroservice/Application/Interfaces/IDeckRepository.cs
@@ -1,6 +1,11 @@
-﻿namespace DeckMicroservice.Application.Interfaces
+﻿using DeckMicroservice.Application.DTOs;
+
+namespace DeckMicroservice.Application.Interfaces
 {
     public interface IDeckRepository
     {
+        Task CreateAsync(CreateDeckRequest deck);
+        Task<List<DeckDto>> GetByOwnerAsync(string ownerId);
+        bool Validate(CreateDeckRequest deck);
     }
 }
