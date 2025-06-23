06a8c545de3ebc274203cf0a41bf39c05028cb63
Chau Denis <chaudenisbreiz@yahoo.fr>
2025-06-23 12:02:18 +0200

    feat(card): Add ServiceCollectionExtension.cs to register services and configure dependency injection

---
+++ CardMicroservice/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,34 @@
+﻿using CardMicroservice.Application.Interfaces;
+using CardMicroservice.Application.UseCases;
+using CardMicroservice.Infrastructure.Config;
+using CardMicroservice.Infrastructure.Mapping;
+using CardMicroservice.Infrastructure.Persistence.Repositories;
+using CardMicroservice.Infrastructure.Scryfall;
+using Microsoft.Extensions.Configuration; 
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CardMicroservice.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddCardMicroserviceServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            // Config Mongo
+            services.Configure<MongoDbConfig>(configuration.GetSection("MongoDb"));
+
+            // Repositories & clients
+            services.AddScoped<ICardRepository, MongoCardRepository>();
+            services.AddHttpClient<IScryfallClient, ScryfallHttpClient>();
+
+            // AutoMapper
+            services.AddAutoMapper(typeof(AutoMapperProfile));
+
+            // UseCases
+            services.AddScoped<GetAllCardsUseCase>();
+            services.AddScoped<GetCardByNameUseCase>();
+            services.AddScoped<ImportCardUseCase>();
+
+            return services;
+        }
+    }
+}
