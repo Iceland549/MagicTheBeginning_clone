45c21069f955da6cc43207d9d493326c0a6fd8c0
Chau Denis <chaudenisbreiz@yahoo.fr>
2025-06-23 13:47:16 +0200

    feat(deck): Add ServiceCollectionExtensions for centralized service registration

---
+++ DeckMicroservice/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,32 @@
+﻿using DeckMicroservice.Application.Interfaces;
+using DeckMicroservice.Application.UseCases;
+using DeckMicroservice.Infrastructure.Config;
+using DeckMicroservice.Infrastructure.Mapping;
+using DeckMicroservice.Infrastructure.Persistence.Repositories;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DeckMicroservice.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddDeckMicroserviceServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            // 1. Configuration MongoDb
+            services.Configure<MongoDbConfig>(configuration.GetSection("MongoDb"));
+
+            // 2. Repository Mongo
+            services.AddScoped<IDeckRepository, MongoDeckRepository>();
+
+            // 3. AutoMapper
+            services.AddAutoMapper(typeof(AutoMapperProfile));
+
+            // 4. UseCases
+            services.AddScoped<CreateDeckUseCase>();
+            services.AddScoped<ValidateDeckUseCase>();
+            services.AddScoped<GetDecksByOwnerUseCase>();
+
+            return services;
+        }
+    }
+}
