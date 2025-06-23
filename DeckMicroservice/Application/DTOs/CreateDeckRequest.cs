a082f370c21a978876b0d62a0c9f6e4a76822ff2
Chau Denis <chaudenisbreiz@yahoo.fr>
2025-06-23 12:55:52 +0200

    feat(deck): Add CardDto for card data transfer

--- DeckMicroservice/Application/DTOs/CreateDeckRequest.cs
+++ DeckMicroservice/Application/DTOs/CreateDeckRequest.cs
@@ -2,5 +2,8 @@
 {
     public class CreateDeckRequest
     {
+        public string OwnerId { get; set; } = null!;   // Player ID (GUID)
+        public string Name { get; set; } = null!;   // Deck name
+        public List<DeckCardDto> Cards { get; set; } = new();   // List of cards
     }
 }
