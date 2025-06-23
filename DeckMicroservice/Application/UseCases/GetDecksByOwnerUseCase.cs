c00af4e2d9fc8d4a7377cfaaa5aa89a28d355696
Chau Denis <chaudenisbreiz@yahoo.fr>
2025-06-23 13:03:53 +0200

    feat(deck): Add GetDecksByOwnerUseCase for retrieving decks by owner

--- DeckMicroservice/Application/UseCases/GetDecksByOwnerUseCase.cs
+++ DeckMicroservice/Application/UseCases/GetDecksByOwnerUseCase.cs
@@ -1,6 +1,15 @@
-﻿namespace DeckMicroservice.Application.UseCases
+﻿using DeckMicroservice.Application.DTOs;
+using DeckMicroservice.Application.Interfaces;
+
+namespace DeckMicroservice.Application.UseCases
 {
     public class GetDecksByOwnerUseCase
     {
+        private readonly IDeckRepository _repo;
+        public GetDecksByOwnerUseCase(IDeckRepository repo) => _repo = repo;
+
+        // Retrieves all decks belonging to a specific owner.
+        public Task<List<DeckDto>> ExecuteAsync(string ownerId) =>
+            _repo.GetByOwnerAsync(ownerId);
     }
 }
