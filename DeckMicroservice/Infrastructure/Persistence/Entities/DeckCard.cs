f2b32004b3659643053a901555f58fa55429ec2c
Chau Denis <chaudenisbreiz@yahoo.fr>
2025-06-23 12:48:37 +0200

    feat(deck): Add DeckCard entity for cards in deck persistence

---
+++ DeckMicroservice/Infrastructure/Persistence/Entities/DeckCard.cs
@@ -0,0 +1,17 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace DeckMicroservice.Infrastructure.Persistence.Entities
+{
+    /// <summary>
+    /// Represents an individual card within a deck.
+    /// </summary>
+    public class DeckCard
+    {
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string CardId { get; set; } = null!;             // CardEntity id or simply the name
+
+        [BsonElement("quantity")]
+        public int Quantity { get; set; }                       // Number of copies of this card
+    }
+}
