f79ca6c30508c6b833d79958d0a5c716c14a7acd
Chau Denis <chaudenisbreiz@yahoo.fr>
2025-06-23 10:22:39 +0200

    feat(card): Add MongoCardRepository for MongoDB card persistence implementation

--- CardMicroservice/Infrastructure/Persistence/MongoCardRepository.cs
+++ CardMicroservice/Infrastructure/Persistence/MongoCardRepository.cs
@@ -1,6 +1,31 @@
-﻿namespace CardMicroservice.Infrastructure.Persistence
+﻿using CardMicroservice.Application.DTOs;
+using CardMicroservice.Application.Interfaces;
+using MongoDB.Driver;
+
+namespace CardMicroservice.Infrastructure.Persistence
 {
-    public class MongoCardRepository
+    public class MongoCardRepository : ICardRepository
     {
+        private readonly IMongoCollection<CardDto> _col;
+
+        public MongoCardRepository(IConfiguration cfg)
+        {
+            var client = new MongoClient(cfg["Mongo:ConnectionString"]);
+            var db = client.GetDatabase(cfg["Mongo:Database"]);
+            _col = db.GetCollection<CardDto>(cfg["Mongo:CardCollection"]);
+        }
+
+        public Task<List<CardDto>> GetAllAsync() =>
+            _col.Find(_ => true).ToListAsync();
+
+        public async Task<CardDto?> GetByNameAsync(string name)
+        {
+            return await _col.Find(c => c.Name.ToLower() == name.ToLower())
+                .FirstOrDefaultAsync(); // Returns a CardDto if found, otherwise null
+        }
+
+
+        public Task AddAsync(CardDto card) =>
+            _col.InsertOneAsync(card);
     }
 }
