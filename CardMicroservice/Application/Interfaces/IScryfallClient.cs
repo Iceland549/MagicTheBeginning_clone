96c8f0d02c43642a738bdcc4b3004302a31f5fd8
Chau Denis <chaudenisbreiz@yahoo.fr>
2025-06-23 09:51:05 +0200

    feat(card): Add IScryfallClient interface for Scryfall API integration

--- CardMicroservice/Application/Interfaces/IScryfallClient.cs
+++ CardMicroservice/Application/Interfaces/IScryfallClient.cs
@@ -1,6 +1,10 @@
-﻿namespace CardMicroservice.Application.Interfaces
+﻿using CardMicroservice.Infrastructure.Scryfall;
+
+namespace CardMicroservice.Application.Interfaces
 {
+    // Récupère le DTO brut de Scryfall
     public interface IScryfallClient
     {
+        Task<ScryfallCardDto?> FetchRawCardByNameAsync(string name);
     }
 }
