94364c05efba726dcb6ef9c63a5e1f8bcc649ab7
Chau Denis <chaudenisbreiz@yahoo.fr>
2025-06-24 08:45:22 +0200

    feat(game): Add StartGameUseCase.cs to handle starting a new game session

--- GameMicroservice/Application/UseCases/StartGameUseCase.cs
+++ GameMicroservice/Application/UseCases/StartGameUseCase.cs
@@ -1,6 +1,29 @@
-﻿namespace GameMicroservice.Application.UseCases
+﻿using GameMicroservice.Application.DTOs;
+using GameMicroservice.Application.Interfaces;
+using System.Threading.Tasks;
+
+namespace GameMicroservice.Application.UseCases
 {
+    /// <summary>
+    /// Use case for starting a new game session between two players.
+    /// </summary>
     public class StartGameUseCase
     {
+        private readonly IGameSessionRepository _repo;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="StartGameUseCase"/> class.
+        /// </summary>
+        /// <param name="repo">The game session repository.</param>
+        public StartGameUseCase(IGameSessionRepository repo) => _repo = repo;
+
+        /// <summary>
+        /// Executes the use case to start a game session.
+        /// </summary>
+        /// <param name="p1">ID of the first player.</param>
+        /// <param name="p2">ID of the second player.</param>
+        /// <returns>The created game session DTO.</returns>
+        public Task<GameSessionDto> ExecuteAsync(string p1, string p2) =>
+            _repo.CreateAsync(p1, p2);
     }
 }
