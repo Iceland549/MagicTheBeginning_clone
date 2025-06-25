349266b1c399c8bbe3359edc5cefb09ae5428c57
Chau Denis <chaudenisbreiz@yahoo.fr>
2025-06-25 17:13:44 +0200

    feat(Game): Implement IDeckClient interface for deck data operations

---
+++ GameMicroservice/Application/Interfaces/IDeckClient.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace GameMicroservice.Infrastructure
+{
+    public interface IDeckClient
+    {
+        Task<List<DeckDto>> GetDecksByOwnerAsync(string ownerId);
+    }
+
+    public class DeckDto
+    {
+        public string OwnerId { get; set; } = null!;
+        public string Name { get; set; } = null!;
+        public List<DeckCardDto> Cards { get; set; } = new List<DeckCardDto>();
+    }
+
+    public class DeckCardDto
+    {
+        public string CardName { get; set; } = null!;
+        public int Quantity { get; set; }
+    }
+}
