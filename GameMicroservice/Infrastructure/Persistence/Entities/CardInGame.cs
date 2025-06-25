7c647db7de3dc1095122201f3503da95ffaf9816
Chau Denis <chaudenisbreiz@yahoo.fr>
2025-06-25 17:15:31 +0200

    feat(Game): Add CardInGame entity for in-game card representation

---
+++ GameMicroservice/Infrastructure/Persistence/Entities/CardInGame.cs
@@ -0,0 +1,23 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace GameMicroservice.Infrastructure.Persistence.Entities
+{
+    public class CardInGame
+    {
+        [BsonElement("cardId")]
+        public string CardId { get; set; } = null!;
+
+        [BsonElement("isTapped")]
+        public bool IsTapped { get; set; }
+
+        [BsonElement("hasSummoningSickness")]
+        public bool HasSummoningSickness { get; set; }
+
+        public CardInGame(string cardId)
+        {
+            CardId = cardId;
+            IsTapped = false;
+            HasSummoningSickness = true;
+        }
+    }
+}
