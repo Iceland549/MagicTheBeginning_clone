556bb00cd779ae6e75d8bebb476d653e0573d74c
Chau Denis <chaudenisbreiz@yahoo.fr>
2025-06-25 17:13:10 +0200

    feat(Game): Introduce PlayerActionDto to handle player actions in game sessions

---
+++ GameMicroservice/Application/DTOs/PlayerActionDto.cs
@@ -0,0 +1,23 @@
+﻿namespace GameMicroservice.Application.DTOs
+{
+    public enum ActionType
+    {
+        Draw,
+        PlayLand,
+        PlayCard,
+        CastInstant,
+        PassToCombat,
+        Attack,
+        PreEnd,
+        EndTurn
+    }
+
+    public class PlayerActionDto
+    {
+        public ActionType Type { get; set; }
+        public string? CardId { get; set; }
+        public List<string>? Attackers { get; set; }
+        public Dictionary<string, string>? Blockers { get; set; }
+        public string? TargetId { get; set; }
+    }
+}
