d84fb18e1e9f9637c7a898ea809a402540e0b32d
Chau Denis <chaudenisbreiz@yahoo.fr>
2025-06-25 17:25:23 +0200

    feat(Game): Integrate service extensions and authentication pipeline in Program.cs

--- GameMicroservice/Program.cs
+++ GameMicroservice/Program.cs
@@ -1,6 +1,15 @@
+using GameMicroservice.Application.Interfaces;
+using GameMicroservice.Application.UseCases;
+using GameMicroservice.Extensions;
+using GameMicroservice.Infrastructure.Mapping;
+using GameMicroservice.Infrastructure.Persistence;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddGameMicroserviceServices(builder.Configuration);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
