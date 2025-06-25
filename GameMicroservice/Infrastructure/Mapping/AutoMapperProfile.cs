d44448d5b02fdccf2f3d03cc42cbeca854681c54
Chau Denis <chaudenisbreiz@yahoo.fr>
2025-06-25 17:24:04 +0200

    feat(Game): Enhance AutoMapperProfile with bidirectional mappings for game entities and DTOs

--- GameMicroservice/Infrastructure/Mapping/AutoMapperProfile.cs
+++ GameMicroservice/Infrastructure/Mapping/AutoMapperProfile.cs
@@ -1,6 +1,22 @@
-﻿namespace GameMicroservice.Infrastructure.Mapping
+﻿using AutoMapper;
+using GameMicroservice.Application.DTOs;
+using GameMicroservice.Infrastructure.Persistence.Entities;
+
+namespace GameMicroservice.Infrastructure.Mapping
 {
-    public class AutoMapperProfile
+    public class AutoMapperProfile : Profile
     {
+        public AutoMapperProfile()
+        {
+            // Entity → DTO
+            CreateMap<GameSession, GameSessionDto>();
+            CreateMap<PlayerState, PlayerStateDto>()
+                .ForMember(dest => dest.ManaPool, opt => opt.MapFrom(src => src.ManaPool));
+
+            // DTO → Entity
+            CreateMap<GameSessionDto, GameSession>();
+            CreateMap<PlayerStateDto, PlayerState>()
+                .ForMember(dest => dest.ManaPool, opt => opt.MapFrom(src => src.ManaPool));
+        }
     }
 }
