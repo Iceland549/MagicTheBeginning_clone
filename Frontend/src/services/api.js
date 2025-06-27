29f2737af2a7415f1ed416b3a55ea903a3db01be
Chau Denis <chaudenisbreiz@yahoo.fr>
2025-06-27 16:20:12 +0200

    refactor(frontend): use environment variable for API base URL and add '/api' prefix

--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -1,13 +1,20 @@
 import axios from 'axios';
 
+// Vérification de l'URL de base de l'API
+// Si l'URL n'est pas définie, on utilise une valeur par défaut
+const API_BASE_URL = process.env.REACT_APP_API_URL || "http://localhost:5000/api";
+
+// Création d'une instance axios avec intercepteur JWT
 const api = axios.create({
-  baseURL: 'http://localhost:5000', // Ocelot Gateway
+  baseURL: API_BASE_URL,
 });
+
 
 api.interceptors.request.use(cfg => {
   const token = localStorage.getItem('accessToken');
   if (token) cfg.headers.Authorization = `Bearer ${token}`;
   return cfg;
-});
+},
+(error) => Promise.reject(error));
 
 export default api;
