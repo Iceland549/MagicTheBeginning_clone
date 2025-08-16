c2572a0f63124e39acbc1b0e55c619be229fbe22
Chau Denis <chaudenisbreiz@yahoo.fr>
2025-08-16 17:26:13 +0200

    feat(game-microservice): add CardInGame mapping

--- GameMicroservice/Infrastructure/Mapping/AutoMapperProfile.cs
+++ GameMicroservice/Infrastructure/Mapping/AutoMapperProfile.cs
@@ -17,6 +17,18 @@ namespace GameMicroservice.Infrastructure.Mapping
             CreateMap<GameSessionDto, GameSession>();
             CreateMap<PlayerStateDto, PlayerState>()
                 .ForMember(dest => dest.ManaPool, opt => opt.MapFrom(src => src.ManaPool));
+
+            CreateMap<CardInGame, CardInGameDto>()
+                .ForMember(dest => dest.ImageUrl, opt => opt.MapFrom(src => src.ImageUrl))
+                .ForMember(dest => dest.TypeLine, opt => opt.MapFrom(src => src.TypeLine))
+                .ForMember(dest => dest.ManaCost, opt => opt.MapFrom(src => src.ManaCost))
+                .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Name));
+
+            CreateMap<CardInGameDto, CardInGame>()
+                .ForMember(dest => dest.ImageUrl, opt => opt.MapFrom(src => src.ImageUrl))
+                .ForMember(dest => dest.TypeLine, opt => opt.MapFrom(src => src.TypeLine))
+                .ForMember(dest => dest.ManaCost, opt => opt.MapFrom(src => src.ManaCost))
+                .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Name));
         }
     }
 }
