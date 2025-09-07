5599c6cd206b97876ec3aca6b15a62ff906bd623
Chau Denis <chaudenisbreiz@yahoo.fr>
2025-09-07 09:27:10 +0200

    test(card-microservice): create GetAllCardsUseCase unit tests

---
+++ Test/CardMicroservice.Tests/GetAllCardsUseCaseTests.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading.Tasks;
+using Xunit;
+using Moq;
+using CardMicroservice.Application.UseCases;
+using CardMicroservice.Application.Interfaces;
+using CardMicroservice.Application.DTOs;
+using System.Collections.Generic;
+
+namespace Test.CardMicroservice.Tests
+{
+    public class GetAllCardsUseCaseTests
+    {
+        [Fact]
+        public async Task ExecuteAsync_ReturnsAllCards()
+        {
+            // Arrange
+            var mockRepo = new Mock<ICardRepository>();
+            var list = new List<CardDto> { new CardDto { Id = "1", Name = "A" }, new CardDto { Id = "2", Name = "B" } };
+            mockRepo.Setup(r => r.GetAllAsync()).ReturnsAsync(list);
+
+            var useCase = new GetAllCardsUseCase(mockRepo.Object);
+
+            // Act
+            var result = await useCase.ExecuteAsync();
+
+            // Assert
+            Assert.Equal(2, result.Count);
+            Assert.Contains(result, c => c.Name == "A");
+        }
+    }
+}
