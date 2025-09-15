48f5ac7ea9b79c24351ae231d86d1c33de126305
Chau Denis <chaudenisbreiz@yahoo.fr>
2025-09-15 12:42:28 +0200

    backend-Auth: add ServiceTokenResponse DTO

---
+++ AuthMicroservice/Application/DTOs/ServiceTokenResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace AuthMicroservice.Application.DTOs
+{
+    public class ServiceTokenResponse
+    {
+        public string AccessToken { get; set; } = null!;
+        public DateTime ExpiresAt { get; set; }
+    }
+}
