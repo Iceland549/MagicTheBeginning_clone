dc8d16f17db5ca1e5285b250d7c53d27514441bc
Chau Denis <chaudenisbreiz@yahoo.fr>
2025-09-15 10:43:13 +0200

    backend-Auth: Add ServiceClient entity for OAuth2 client credentials flow

---
+++ AuthMicroservice/Infrastructure/Persistence/Entities/ServiceClient.cs
@@ -0,0 +1,10 @@
+﻿namespace AuthMicroservice.Infrastructure.Persistence.Entities
+{
+    public class ServiceClient
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string ClientId { get; set; } = null!;
+        public string ClientSecretHash { get; set; } = null!;
+        public string AllowedScopes { get; set; } = "";
+    }
+}
