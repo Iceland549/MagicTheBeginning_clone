fd18acabffcd967a154e12f5901772e9b88ee09c
Chau Denis <chaudenisbreiz@yahoo.fr>
2025-09-15 13:15:47 +0200

    backend-Auth: add SeedServiceClientsUseCase for seeding trusted service clients

---
+++ AuthMicroservice/Application/UseCases/SeedServiceClientsUseCase.cs
@@ -0,0 +1,39 @@
+﻿using AuthMicroservice.Application.Interfaces;
+using AuthMicroservice.Infrastructure.Persistence.Entities;
+using System.Threading.Tasks;
+
+namespace AuthMicroservice.Application.UseCases
+{
+    public class SeedServiceClientsUseCase
+    {
+        private readonly IServiceClientRepository _repo;
+
+        public SeedServiceClientsUseCase(IServiceClientRepository repo)
+        {
+            _repo = repo;
+        }
+
+        public async Task ExecuteAsync()
+        {
+            if (!await _repo.ExistsAsync("game-service"))
+            {
+                await _repo.AddAsync(new ServiceClient
+                {
+                    ClientId = "game-service",
+                    ClientSecretHash = BCrypt.Net.BCrypt.HashPassword("SuperSecretGame"),
+                    AllowedScopes = "deck.read"
+                });
+            }
+
+            if (!await _repo.ExistsAsync("deck-service"))
+            {
+                await _repo.AddAsync(new ServiceClient
+                {
+                    ClientId = "deck-service",
+                    ClientSecretHash = BCrypt.Net.BCrypt.HashPassword("SuperSecretDeck"),
+                    AllowedScopes = "deck.read,deck.write"
+                });
+            }
+        }
+    }
+}
